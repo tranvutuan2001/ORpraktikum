3ac059d948518a594adc7acdf159cf596bdf384e
Xiaotong Meng <xiaotong.meng@rwth-aachen.de>
2022-05-14 21:04:10 +0200

    .

--- gurobi/test_model1.py
+++ gurobi/test_model1.py
@@ -1,4 +1,5 @@
 
+from cgi import print_form
 from gurobipy import *
 
 eoncost = 0.437   # euro per kwh
@@ -9,32 +10,32 @@ def solve():
     buildings = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"]
     heatpumps = ["H1", "H2", "H3", "H4", "H5"]
 
+
     # {"B1:100, "B2":200,...,"B10":1000}
     benefits = {buildings[i]: i*100 for i in range(len(buildings))}
     # {"H1":1, "H2":2,...,"H5":5}
     costs = {heatpumps[i]: i+1 for i in range(len(heatpumps))}
     area = {buildings[i]: i*40 for i in range(len(buildings))}
     demand = {buildings[i]: i for i in range(len(buildings))}
-    P = {}
+    # P = {}
 
     model = Model("Heatpumps")
     # for x in heatpumps:
     #     costs[x] = installation_cost[x] + cost_of_electric[x]
 
-    # Variables
-    for b in buildings:
-        for h in heatpumps:
-            P[(h, b)] = model.addVar(vtype=GRB.BINARY, name=f'P({h},{b})')
-    for b in buildings:
-        model.addConstr(quicksum(P[(h, b)] for h in heatpumps) <= 1)
-
-    # Constraints
+    # Variables   
+    P = model.addVars(heatpumps, buildings, name = "P" )
+   
+    
+     # Constraints
+    model.addConstrs(P.sum(h, '*') <=1 for h in heatpumps )
     model.addConstr(quicksum(P[(h, b)]*benefits[b]
-                             for h in heatpumps for b in buildings) >= 2000)
+                             for h in heatpumps for b in buildings) >= 3000)
 
     # Objective
-    model.setObjective(quicksum(P[(h, b)] * (costs[h] + area[b]*demand[b]*eoncost )
-                       for h in heatpumps for b in buildings), GRB.MINIMIZE)
+    obj = quicksum(P[(h, b)] * (costs[h] + area[b]*demand[b]*eoncost )
+                       for h in heatpumps for b in buildings)
+    model.setObjective(obj, GRB.MINIMIZE)
     model.update()
     model.optimize()
     model.write("model.lp")
@@ -44,7 +45,9 @@ def solve():
             print(v.varName)
 
             
-
+            
+            
 solve()
+
 
 
