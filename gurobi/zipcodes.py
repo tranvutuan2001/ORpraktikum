fdb7786740b5981002db09627670cec4f70e3e74
lakhoune <a.b.lakhoune@gmail.com>
2022-05-31 23:03:13 +0200

    improve perf. by limiting req

--- gurobi/zipcodes.py
+++ gurobi/zipcodes.py
@@ -22,16 +22,24 @@ df = pd.read_excel(ACOOLHEAD, engine='openpyxl')
 weather_file_col = df['weather file names']
 zipcodes = [None]*len(weather_file_col)
 df['zipcode'] = zipcodes
+knownzipcodes = {}
 print(df.head())
 for i in range(len(weather_file_col)):
-    if get_zipcode_from_df(df, i) is None:
-        # Get the zipcode from the coordinates by calling the geolocator api. This is a slow process as we need to limit the number of requests to 1 per second.
+    zipcode = get_zipcode_from_df(df, i)
+    if zipcode is None:
         coords = weather_file_col[i].split('_')[1]
         lat = coords[0:2] + '.' + coords[2:6]  # 54.3288
         lng = coords[6:8] + '.' + coords[8:12]  # 10.1350
-        print("request for zipcode with coordinates: " +
-              str(lat) + " " + str(lng))
-        zipcodes[i] = get_zipcode_by_coords(lat, lng)
-        df['zipcode'][i] = zipcodes[i]
-        df.to_excel(os.path.join(
+        if (lat, lng) in knownzipcodes:
+            zipcode = knownzipcodes[(lat, lng)]
+        else:
+            # Get the zipcode from the coordinates by calling the geolocator api. This is a slow process as we need to limit the number of requests to 1 per second.
+            print("request for zipcode with coordinates: " +
+                  str(lat) + " " + str(lng))
+            zipcode = get_zipcode_by_coords(lat, lng)
+            knownzipcodes[(lat, lng)] = zipcode
+            df.to_excel(os.path.join(
+                dirname, "../data-sources/ACoolHeadZipCodes.xls"), index=False)
+    df['zipcode'][i] = zipcode
+df.to_excel(os.path.join(
             dirname, "../data-sources/ACoolHeadZipCodes.xls"), index=False)
