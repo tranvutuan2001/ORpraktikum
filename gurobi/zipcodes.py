b4743f232feb5416c92af5c1d7681e763da20702
lakhoune <a.b.lakhoune@gmail.com>
2022-06-01 00:08:38 +0200

    improve script

--- gurobi/zipcodes.py
+++ gurobi/zipcodes.py
@@ -1,8 +1,10 @@
+from cmath import nan
 from utilities import get_zipcode_by_coords
 import pandas as pd
 import os
 import xlwt
-
+from tqdm import tqdm
+import numpy as np
 dirname = os.path.dirname(__file__)
 ACOOLHEAD = os.path.join(dirname, "../data-sources/ACoolHeadZipCodes.xls")
 
@@ -20,13 +22,17 @@ def get_zipcode_from_df(df, i):
 df = pd.read_excel(ACOOLHEAD)
 
 weather_file_col = df['weather file names']
-zipcodes = [None]*len(weather_file_col)
-df['zipcode'] = zipcodes
 knownzipcodes = {}
-print(df.head())
-for i in range(len(weather_file_col)):
+df.fillna(value=0, inplace=True)
+for i in tqdm(range(len(weather_file_col))):
+    if(i % 1000 == 0):
+        df.to_excel(os.path.join(
+            dirname, "../data-sources/ACoolHeadZipCodes.xls"), index=False)
+    if not df['zipcode'][i] == 0:
+        # print(df['zipcode'][i])
+        continue
     zipcode = get_zipcode_from_df(df, i)
-    if zipcode is None:
+    if zipcode == 0:
         coords = weather_file_col[i].split('_')[1]
         lat = coords[0:2] + '.' + coords[2:6]  # 54.3288
         lng = coords[6:8] + '.' + coords[8:12]  # 10.1350
@@ -34,14 +40,12 @@ for i in range(len(weather_file_col)):
             zipcode = knownzipcodes[(lat, lng)]
         else:
             # Get the zipcode from the coordinates by calling the geolocator api. This is a slow process as we need to limit the number of requests to 1 per second.
-            print("request for zipcode with coordinates: " +
-                  str(lat) + " " + str(lng))
+            # print("request for zipcode with coordinates: " +
+            #       str(lat) + " " + str(lng))
             zipcode = get_zipcode_by_coords(lat, lng)
             knownzipcodes[(lat, lng)] = zipcode
 
-    df['zipcode'][i] = zipcode
-    if(i % 10 == 0):
-        df.to_excel(os.path.join(
-        dirname, "../data-sources/ACoolHeadZipCodes.xls"), index=False)
+    df.loc[df['zipcode'][i]] = zipcode
+
 df.to_excel(os.path.join(
             dirname, "../data-sources/ACoolHeadZipCodes.xls"), index=False)
