2a28157c8b5bf221ceef65f18986d55c4075287a
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-06-13 20:46:41 +0200

    add operating districts and script

--- gurobi/operating.radius.py
+++ gurobi/operating.radius.py
@@ -5,33 +5,55 @@ import os
 from tqdm import tqdm
 dirname = os.path.dirname(__file__)
 
-MAX_OPERATING_RADIUS = 90  # in km
+MAX_OPERATING_RADIUS = 100  # in km
 distances = {}
 known_distances = {}
 
 
 def add_operating_radius():
     # if file exists, load it
+    # # if os.path.isfile(os.path.join(
+    # #         dirname, "data-sources/Distributor_data_with_radius.csv")):
+    # #     DISTRIBUTERS_DATA = os.path.join(
+    # #         dirname, "data-sources/Distributor_data_with_radius.csv")
+    # #     df = pd.read_csv(DISTRIBUTERS_DATA)
+    # #     if(not 'operating radius' in df.columns):
+    # #         df['operating radius'] = None
+    # #     # if not, create it from data_from_Hannah_with_coordinates.csv
+    # else:
+    DISTRIBUTERS_DATA = os.path.join(
+        dirname, "data-sources/Distributor_data.csv")
+    df = pd.read_csv(DISTRIBUTERS_DATA)
+    df['operating radius'] = None
+    df['operating radius'] = df['operating radius'].fillna(0)
+    df.to_csv(os.path.join(
+        dirname, "data-sources/Distributor_data_with_radius.csv"), index=False)
+
+    df['operating radius'] = df['operating radius'].astype(int)
+
+    for i in tqdm(range(len(df))):
+        df.iloc[i, df.columns.get_loc('operating radius')] = 100
+
+    df.to_csv(os.path.join(
+        dirname, "data-sources/Distributor_data_with_radius.csv"), index=False)
+
+
+def add_operating_districts():
+    # if file exists, load it
     if os.path.isfile(os.path.join(
             dirname, "data-sources/Distributor_data_with_radius.csv")):
         DISTRIBUTERS_DATA = os.path.join(
             dirname, "data-sources/Distributor_data_with_radius.csv")
         df = pd.read_csv(DISTRIBUTERS_DATA)
-        if(not 'operating_regions' in df.columns):
-            df['operating_regions'] = None
+        if(not 'operating districts' in df.columns):
+            df['operating districts'] = None
         # if not, create it from data_from_Hannah_with_coordinates.csv
     else:
-        DISTRIBUTERS_DATA = os.path.join(
-            dirname, "data-sources/Distributor_data.csv")
-        df = pd.read_csv(DISTRIBUTERS_DATA)
-        df['operating_regions'] = None
-        df['operating_regions'] = df['operating_regions'].astype(object)
-        df.to_csv(os.path.join(
-            dirname, "data-sources/Distributor_data_with_radius.csv"), index=False)
+        raise Exception("Distributor_data_with_radius.csv not found")
 
-    df['operating_regions'] = df['operating_regions'].astype(object)
+    df['operating districts'] = df['operating districts'].astype(object)
     # get missing operating regions
-    missing_operating_regions = df[df['operating_regions'].isna()]
+
     # get the districts from the ACOOLHEAD data
     ACOOLHEAD = os.path.join(
         dirname, "data-sources/data_from_Hannah_with_coordinates_and_zipcodes.csv")
@@ -41,14 +63,15 @@ def add_operating_radius():
         'Administrative district')
 
     # get the operating regions for each missing row
-    for i in tqdm(missing_operating_regions.index):
-        zipcode = missing_operating_regions.loc[i, 'zipcode']
-        regions = [key for key in get_operating_regions(
-            districts, str(zipcode))]
+    for i in tqdm(df.index):
+        zipcode = df.loc[i, 'zipcode']
+        radius = df.loc[i, 'operating radius']
+        regions = [key for key in get_operating_districts(
+            districts, str(zipcode), radius).keys()]
         df.iloc[i, df.columns.get_loc(
-            'operating_regions')] = ';'.join(regions)
+            'operating districts')] = ';'.join(regions)
 
-        # missing_operating_regions['operating_regions'][i] = np.array([
+        # missing_operating_regions['operating districts'][i] = np.array([
         #     key for key in regions.keys()], dtype=object)
         df.to_csv(os.path.join(
             dirname, "data-sources/Distributor_data_with_radius.csv"), index=False)
@@ -58,7 +81,7 @@ def add_operating_radius():
         dirname, "data-sources/Distributor_data_with_radius.csv"), index=False)
 
 
-def get_operating_regions(districts, zipcode):
+def get_operating_districts(districts, zipcode, op_radius=MAX_OPERATING_RADIUS):
     zipcode = '0'+str(zipcode) if len(str(zipcode)) == 4 else str(zipcode)
     regions = {}
 
@@ -67,6 +90,10 @@ def get_operating_regions(districts, zipcode):
         zipcode_of_district = '0' + \
             str(zipcodes[0]) if len(
                 str(zipcodes[0])) == 4 else str(zipcodes[0])
+
+        if op_radius > 700:
+            regions[district] = 1
+
         if zipcode_of_district == zipcode:
             regions[district] = 0
             continue
@@ -86,10 +113,11 @@ def get_operating_regions(districts, zipcode):
                 'postalcode': zipcode_of_district, 'country': 'Germany'})
             known_distances[zipcode_of_district, zipcode] = distance
 
-        if not distance == None and distance < MAX_OPERATING_RADIUS:
-            regions[district] = distance
+            if not distance == None and distance < op_radius:
+                regions[district] = distance
 
     return regions
 
 
-add_operating_radius()
+# add_operating_radius()
+add_operating_districts()
