76e808c1e4740f0dad592d3adfac443e89ce2b4b
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-06-24 16:17:02 +0200

    Revert "determine the possible configuration beforehand"
    This reverts commit 8d781524a81774d88314cb1efa3e8d55c20628de.

--- gurobi/modelsolver.py
+++ gurobi/modelsolver.py
@@ -49,40 +49,26 @@ def solve(districts, heatpumps, housing, fitness, distributors, NUMBER_OF_YEARS,
     # Create a new model
     print("Preparing the model\n")
     model = Model("Heatpumps")
-
-    # Determine possible configurations
-    print("Determining possible configurations\n")
-    start = timeit.default_timer()
-
-    configurations = get_configurations(heatpumps, housing, distributors, T) # list of all possible configurations
-    print(f"{len(configurations)} possible configurations")
-    heatpump_index = list(dict.fromkeys(
-        configuration[0] for configuration in configurations)) # an index of all possbile heat pumps
-    housing_index = list(dict.fromkeys(
-        configuration[1] for configuration in configurations)) # an index of all possible housings
-    distributor_index = list(dict.fromkeys(
-        configuration[2] for configuration in configurations)) # an index of all possible distributors
-    # currently no filtering done but maybe later there could be a time filter
-    # time_index = list(dict.fromkeys(configuration[3] for configuration in configurations)) # an index of all possible times
-
-    stop = timeit.default_timer()
-    print('Time to determine configurations: ',
-          f"{round(stop - start, 2)} seconds\n")
-
     # Add Variables
     print("Adding variables")
     start = timeit.default_timer()
-    print(len(configurations), "variables will be added")
+    print(len(heatpumps) * len(housing) * T *
+          len(distributors), "variables will be added")
     # Quantity of installed heat pumps with given conditions
     x = {}
     # TODO: only add the variables for house/heatpump combinations that are feasible, do not add variables if fitness=0
-    for m in heatpump_index:
-        for i in housing_index:
-            for t in range(T):
-                for d in distributor_index:
-                    x[m, i, t, d] = model.addVar(vtype='I',
-                                                 name=f'hp_type_{str(m)}_at_house_type_{str(i)}_in_year_{str(t)}_by_distributor_{str(distributors[d]["name"])}'
-                                                 )
+    for m in heatpumps:
+        for i in housing:
+            if fitness[i,m]==0:
+                for t in range(T):
+                    for d in distributors:
+                        x[m,i,t,d]=0
+            else:
+                for t in range(T):
+                    for d in distributors:
+                        x[m, i, t, d] = model.addVar(vtype='I',
+                        name=f'hp_type_{str(m)}_at_house_type_{str(i)}_in_year_{str(t)}_by_distributor_{str(distributors[d]["name"])}'
+                    )
     stop = timeit.default_timer()
     print('Time to add the variables: ', f"{round(stop - start, 2)} seconds\n")
 
@@ -98,26 +84,34 @@ def solve(districts, heatpumps, housing, fitness, distributors, NUMBER_OF_YEARS,
                         model.addConstr(x[m, i, t, d] == 0, name="C1")
     """
     # Constraint 2:  Install heat pumps in AT LEAST the specified percentage of all houses
-    house_count = sum(housing[i]['quantity'] for i in housing_index)
+    house_count = sum(housing[i]['quantity'] for i in housing)
     model.addConstr(
-        quicksum(x[m, i, t, d] for m in heatpump_index for i in housing_index for t in range(
-            T) for d in distributor_index) >= MIN_PERCENTAGE * house_count, name="C2"
+        quicksum(x[m, i, t, d] for m in heatpumps for i in housing for t in range(
+            T) for d in distributors) >= MIN_PERCENTAGE * house_count, name="C2"
     )
 
     # Constraint 3: Only install as many heatpumps in a house category as the total quantity of houses of that type
-
-    model.addConstrs(
-        (quicksum(x[m, i, t, d] for m in heatpump_index for t in range(T)
-                  for d in distributor_index) <= housing[i]['quantity'] for i in housing_index), name="C3"
-    )
+    for i in housing:
+        model.addConstr(
+            quicksum(x[m, i, t, d] for m in heatpumps for t in range(T)
+                     for d in distributors) <= housing[i]['quantity'], name="C3"
+        )
 
     # Constraint 4: Only install up to the current expected sales volume
-    model.addConstrs((quicksum(
-        x[m, i, t, d] for i in housing_index for m in heatpump_index for d in distributor_index) <= max_sales[t] for t in range(T)), name="C4")
+    for t in range(T):
+        model.addConstr(quicksum(
+            x[m, i, t, d] for i in housing for m in heatpumps for d in distributors) <= max_sales[t], name="C4")
 
     # Constraints 5: Respect the operation radius for each distributor
-    # not needed since we already have this in get configurations
-
+    # TODO: add the constraint 5 as explained above
+    for d in distributors:
+        for i in housing:
+            dist = cal_dist((housing[i]['lat'], housing[i]['long']),
+                            (distributors[d]['lat'], distributors[d]['long']))
+            if dist > 2000:
+                for m in heatpumps:
+                    for t in range(T):
+                        model.addConstr(x[m, i, t, d] == 0, name="C5")
 
     # Constraint 6: Respect max_installations capacity
     # TODO: implement the constraint "yearly workforce <= qty of heat pumps installed by the distributor"
@@ -136,18 +130,16 @@ def solve(districts, heatpumps, housing, fitness, distributors, NUMBER_OF_YEARS,
           installations of heat pumps.
     """
     # TODO: find a better cost function : lifespan of boiler/heatpumps, total cost of ownership
-    obj = quicksum((
-        x[m, i, t, d] * heatpumps[m]['price']
-
+    obj = quicksum((x[m, i, t, d] * heatpumps[m]['price']
                     + quicksum(x[m, i, t_1, d] * (ELECTRICITY_COST_PER_UNIT * electr_timefactor[t_1]
                                                   + CO2_EMISSION_EON * CO2_EMISSION_PRICE * CO2_timefactor[t_1])
                                * housing[i]['average heat demand'] / heatpumps[m]['cop'] for t_1 in range(t + 1))
-
                     + (housing[i]['quantity'] - quicksum(x[m, i, t_1, d]
                        for t_1 in range(t + 1)))
-        * (AVERAGE_BOILER_COST_PER_UNIT * gas_timefactor[t] + CO2_EMISSION_GAS * CO2_EMISSION_PRICE * CO2_timefactor[t])
-        * housing[i]['average heat demand'] / BOILER_EFFICIENCY)
-                   for i in housing_index for m in heatpump_index for d in distributor_index for t in range(T))
+                    * (AVERAGE_BOILER_COST_PER_UNIT * gas_timefactor[t]
+                       + CO2_EMISSION_GAS * CO2_EMISSION_PRICE * CO2_timefactor[t])
+                    * housing[i]['average heat demand'] / BOILER_EFFICIENCY) 
+                   for i in housing for m in heatpumps for d in distributors for t in range(T))
     model.setObjective(obj, GRB.MINIMIZE)
 
     stop = timeit.default_timer()
@@ -185,35 +177,3 @@ def solve(districts, heatpumps, housing, fitness, distributors, NUMBER_OF_YEARS,
    
     return
     """
-
-
-def get_configurations(heatpumps, housing, distributors, T, operating_radius=2000):
-    """Generate the possible configurations for the model.
-       A configuration is possible only if the heatpump can fulfill the demand of the house and the house is in the operating radius of the distributor.
-
-    Args:
-        heatpumps (dict)
-        housing (dict)
-        distributors (dict)
-        T (dict)
-        operating_radius (int, optional). Defaults to 2000.
-
-    Returns:
-        _type_: _description_
-    """
-    configurations = []
-    for m in heatpumps:
-        for i in housing:
-            produced_heat = heatpumps[m]['produced heat']
-            max_heat_demand = housing[i]['max_heat_demand_W/m^2']
-            if produced_heat >= max_heat_demand:
-                for d in distributors:
-                    dist = cal_dist((housing[i]['lat'], housing[i]['long']),
-                                    (distributors[d]['lat'], distributors[d]['long']))
-                    if dist <= operating_radius:
-                        for t in range(T):
-                            configurations.append((m, i, d, t))
-    initial_count = len(heatpumps) * len(housing) * len(distributors) * T
-    print("Variable set reduced to", round(
-        len(configurations)/initial_count * 100, 3), "%\n")
-    return configurations
