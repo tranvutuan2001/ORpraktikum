98274fbed829ff6cb8a9a27848a2065724096f42
PatrickSchro <105563390+PatrickSchro@users.noreply.github.com>
2022-06-26 18:13:45 +0200

    Add files via upload

---
+++ gurobi/dataprp.py
@@ -0,0 +1,162 @@
+import pandas as pd
+from gurobipy import *
+import timeit
+import re
+import os
+from utilities import cal_dist
+
+
+dirname = os.path.dirname(__file__)
+ACOOLHEAD = os.path.join(dirname, './data-sources/data_from_Hannah_with_coordinates_zipcodes_heatcapacity_positive_building_count.csv')
+DISTRIBUTOR = os.path.join(dirname, './data-sources/Distributor_data_with_coordinates.csv')
+HEAT_PUMPS = os.path.join(dirname, './data-sources/heat_pumps_air_water_price.csv')
+FPOWDATA = os.path.join(dirname, './data-sources/fpow.csv')
+PARAMETERS = os.path.join(dirname, './data-sources/parameters.xlsx')
+
+
+
+# function calls and combines all other function except for prepare_params
+def data_preprocess(T, operating_radius):
+    print("Prepare the data")
+    start = timeit.default_timer()
+
+    housing_dataframe = pd.read_csv(ACOOLHEAD)
+    heatpump_dataframe = pd.read_csv(HEAT_PUMPS)
+    distributors_dataframe = pd.read_csv(DISTRIBUTOR)
+
+    housing_data = prepare_housing_data(
+        housing_dataframe, max_entries=None, zipcodes_of_interest="^(5[0-3])"
+    )
+    districts = get_districts(housing_dataframe)
+    heatpump_data = prepare_heatpump_data(heatpump_dataframe, max_entries=4)
+    distributor_data = prepare_distributor(
+        distributors_dataframe, zipcodes_of_interest="^(5[0-3])", max_entries=10
+    )
+    
+    fitness_data = prepare_fitness_on_run_time(heatpump_data, housing_data)
+
+    stop = timeit.default_timer()
+    print('Time to prepare the data: ', round(stop - start, 2), "s\n")
+
+    configurations = get_configurations(heatpump_data, housing_data, distributor_data, T, operating_radius)
+
+    return districts, heatpump_data, housing_data, fitness_data, distributor_data, configurations
+
+
+def prepare_fitness_on_run_time(M, I):
+    fitness = {}
+    for i in I:
+        for m in M:
+            produced_heat = M[m]['produced heat']
+            max_heat_demand = I[i]['max_heat_demand_W/m^2']
+
+            if max_heat_demand <= produced_heat:
+                # this means the heatpump matches our heat demand
+                fitness[(i, m)] = 1
+            else:
+                fitness[(i, m)] = 0
+    return fitness
+
+
+def prepare_housing_data(df, RADIUS_OF_INTEREST=None, max_entries=None, zipcodes_of_interest=None):
+    housing_data = {i:
+        {
+            "district": df["Administrative district"][i],
+            "type of building": df["Type of building"][i],
+            "quantity": int(round(df["Number of buildings"][i], 0)),
+            "Surface area": int(df["Surface area [m^2]"][i]),
+            "modernization status": df["modernization status"][i],
+            "max heat demand [kWh/m^2]": df["max heat demand [kWh/m^2]"][i],
+            "average heat demand": df["average heat demand [kWh/m^2]"][i]*int(df["Surface area [m^2]"][i]) ,
+            "Heatcapacity": df["Heatcapacity"][i],
+            "year of construction": df["Year of construction"][i],
+            "max_heat_demand_W/m^2": round(int(df["Surface area [m^2]"][i]) * df["Heatcapacity"][i] / 1000, 2),
+            'long': df['long'][i],
+            'lat': df['lat'][i],
+            'zipcode': df['zipcode'][i]
+        }
+        for i in range(len(df["long"]))
+        if len(str(df["zipcode"][i])) == 5 and re.match(zipcodes_of_interest, str(df["zipcode"][i]))
+    }
+
+    if max_entries is None:
+        return {i: list(housing_data.values())[i] for i in range(
+            len(housing_data.values()))}
+    else:
+        return {i: list(housing_data.values())[i] for i in range(
+            len(housing_data.values())) if i < max_entries}
+
+
+def prepare_heatpump_data(df_hp, max_entries=None):
+    df_hp = df_hp.sort_values(by=['Heat output A2/W35 (kW)'], ascending=False)
+    heatpump_data = {i:
+        {
+            'brand_name': df_hp['hp_name'][i],
+            'cop': df_hp['COP A2/W35'][i],
+            'produced heat': df_hp['Heat output A2/W35 (kW)'][i],
+            'price': df_hp['price'][i]
+        }
+        for i in range(len(df_hp['hp_name']))}
+    if max_entries is not None:
+        return {i: list(heatpump_data.values())[i] for i in range(
+            len(heatpump_data.values())) if i < max_entries}
+    return heatpump_data
+
+
+def get_districts(df):
+    return df['Administrative district'].unique()
+
+
+def prepare_distributor(df, RADIUS_OF_INTEREST=20, zipcodes_of_interest=None, max_entries=None):
+    distributors = {
+        i: {
+            'name': df['Distributors'][i],
+            'long': df['long'][i],
+            'lat': df['lat'][i]
+            #TODO: later add max_installations
+        }
+        for i in range(len(df))
+        if len(str(df["zipcode"][i])) == 5 and re.match(zipcodes_of_interest, str(df["zipcode"][i]))
+    }
+    if max_entries is None:
+        return distributors
+    else:
+        return {i: list(distributors.values())[i] for i in range(
+            len(distributors.values())) if i < max_entries}
+
+def get_configurations(heatpumps, housing, distributors, T, operating_radius):
+    """Generate the possible configurations for the model.
+       A configuration is possible only if the heatpump can fulfill the demand of the house and the house is in the operating radius of the distributor.
+
+    Args:
+        heatpumps (dict)
+        housing (dict)
+        distributors (dict)
+        T (dict)
+        operating_radius (int, optional). Defaults to 2000.
+
+    Returns:
+        _type_: _description_
+    """
+    configurations = tuplelist()
+    for m in heatpumps:
+        for i in housing:
+            produced_heat = heatpumps[m]['produced heat']
+            max_heat_demand = housing[i]['max_heat_demand_W/m^2']
+            if produced_heat >= max_heat_demand:
+                for d in distributors:
+                    dist = cal_dist((housing[i]['lat'], housing[i]['long']),
+                                    (distributors[d]['lat'], distributors[d]['long']))
+                    if dist <= operating_radius:
+                        for t in range(T):
+                            configurations.append((m, i, d, t))
+    initial_count = len(heatpumps) * len(housing) * len(distributors) * T
+    print("Variable set reduced to", round(
+        len(configurations) / initial_count * 100, 3), "%\n")
+
+    for m in heatpumps:
+        for i in housing:
+            for d in distributors:
+                configurations.append((m, i, d, -1))
+
+    return configurations
---
+++ gurobi/modelsolver.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+# Template for our final model
+
+from gurobipy import Model, quicksum, GRB, tuplelist
+import os
+from printsolution import write_solution_csv
+import timeit
+
+
+dirname = os.path.dirname(__file__)
+
+
+def solve(districts, heatpumps, housing, fitness, distributors, NUMBER_OF_YEARS, MIN_PERCENTAGE,
+          CO2_EMISSION_GAS, CO2_EMISSION_EON, BOILER_EFFICIENCY, CO2_EMISSION_PRICE,
+          max_sales, AVERAGE_BOILER_COST_PER_UNIT, ELECTRICITY_COST_PER_UNIT,
+          electr_timefactor, gas_timefactor, CO2_timefactor, configurations):
+    """Solves the heat pump problem.
+
+    NUMBER OF YEARS (int): number of years to be considered for the model 
+    heatpumps (dict) : dictionary of heat pumps, each containing the keys:
+        'brand_name' (str) : brand name of the heat pump
+        'cop' (float) : COP (A2/W35) of the heat pump 
+        'produced heat' (float) : heat produced by the heat pump
+        'price'(float) : price of heat pump including installation and accessories
+    housing (dict) : dictionary of buildings each containing the keys:
+        "type of building" (str) : building type
+        "modernization_status" (str) : status of the building (i.e. whether it is modernized or not)
+        "max_heat_demand_W/m^2" (int)  : Standard heating load according to DIN12831 in the form of standard values from technical literature/BWP w/m^2
+        "district" (str) : district of the building
+        "quantity" (int) : number of buildings of the same type in the district
+        "Surface area" (float) : size of residental space
+        "average heat demand" (float): demand kwh/m^2
+        "year of construction" (int): year of construction
+        "long" (float): longitude of house
+        "lat" (float): latitude of house
+    distributors (dict) : dictionary of heat pump installers 
+        "name" (str): name of the company
+        "long" (str): longitude of company location
+        "lat" (str): latitude of company location
+        "zipcode" (int) : zipcode
+        "max_installations" (int): #TODO amount of possible total heat pump installation by the company per year
+
+    __________________________________________________________________________________________________________
+
+    """
+
+    T = NUMBER_OF_YEARS
+    # Create a new model
+    print("Preparing the model\n")
+    model = Model("Heatpumps")
+
+    P = configurations
+    index_heatpump = set(k[0] for k in P)
+    index_housing = set(k[1] for k in P)
+    index_distributor = set(k[2] for k in P)
+    index_time = set(k[3] for k in P)
+    # Add Variables
+    print("Adding variables")
+    start = timeit.default_timer()
+    print(len(P), "variables will be added")
+    # Quantity of installed heat pumps with given conditions
+    x = {}
+    for p in P:
+        if p[3] >= 0:
+            x[p] = model.addVar(vtype='I', lb=0,
+                                         name=f'hp_type_{str(p[0])}_at_house_type_{str(p[1])}_by_distributor_{str(distributors[p[2]]["name"])}_in_year_{str(p[3])}')
+        else: x[p] = 0
+
+    stop = timeit.default_timer()
+    print('Time to add the variables: ', f"{round(stop - start, 2)} seconds\n")
+
+    print("Adding the constraints")
+    start = timeit.default_timer()
+
+    # Constraint 1: Consider Installability : never assign Heat Pump to a housetype if they do not fit to the house type
+    # removed because this is handled by get_configurations
+
+    # Constraint 2:  Install heat pumps in AT LEAST the specified percentage of all houses
+    house_count = sum(housing[i]['quantity'] for i in housing)
+    model.addConstr(
+        quicksum(x[p] for p in P.select('*', '*', '*', T-1)) >= MIN_PERCENTAGE * house_count, name="C2"
+    )
+
+    # Constraint 3: Only install as many heatpumps in a house category as the total quantity of houses of that type
+
+    for i in index_housing:
+        model.addConstr((quicksum(x[p] for p in P.select("*", i, "*", T-1)) <= housing[i]['quantity']), name="C3")
+
+    # Constraint 4: Only install up to the current expected sales volume
+    for t in range(T):
+        model.addConstr((quicksum(
+            x[p] for p in P.select("*", "*", "*", t)) - quicksum(x[p] for p in P.select("*", "*", "*", t-1)) <= max_sales[t]), name="C4")
+
+    # Constraints 5: Respect the operation radius for each distributor
+    # removed because this is handled by get_configurations
+
+    # Constraint 6: Respect max_installations capacity
+    # TODO: implement the constraint "yearly workforce <= qty of heat pumps installed by the distributor"
+
+    # Constraint 7: x[p] is a cumulative value
+    for t in range(T):
+        model.addConstr((quicksum(
+            x[p] for p in P.select("*", "*", "*", t)) - quicksum(x[p] for p in P.select("*", "*", "*", t-1)) >= 0), name="C7")
+
+    stop = timeit.default_timer()
+    print("Time to add the constraints: ",
+          f"{round(stop - start, 2)} seconds\n")
+
+    # Add Objective
+    print("Adding objective function")
+    start = timeit.default_timer()
+    # TODO: add other objective function
+
+    """
+          We calculate and sum yearly operation costs for each house and add the costs for inital
+          installations of heat pumps.
+    """
+    # TODO: find a better cost function : lifespan of boiler/heatpumps, total cost of ownership
+    obj = quicksum(x[p] * heatpumps[p[0]]['price'] for p in P.select('*', '*', '*', T-1)) \
+                    + quicksum(x[p] * ((ELECTRICITY_COST_PER_UNIT * electr_timefactor[p[3]]
+                                                 + CO2_EMISSION_EON * CO2_EMISSION_PRICE * CO2_timefactor[p[3]])
+                             / heatpumps[p[0]]['cop']
+                    - (AVERAGE_BOILER_COST_PER_UNIT * gas_timefactor[p[3]]
+                       + CO2_EMISSION_GAS * CO2_EMISSION_PRICE * CO2_timefactor[p[3]])
+                    / BOILER_EFFICIENCY)* housing[p[1]]['average heat demand']
+                   for p in P)
+    model.setObjective(obj, GRB.MINIMIZE)
+#    for p in P:
+#        print(p[2])
+    stop = timeit.default_timer()
+    print('Time to add the objective function: ',
+          f"{round(stop - start, 2)} seconds\n")
+    model.update()
+
+    # Solve Model
+    print("Solving the model")
+    start = timeit.default_timer()
+    model.write(os.path.join(dirname, "solutions\model.lp"))
+    model.optimize()
+
+    # Print model details and solution
+    # model.computeIIS()
+    # model.write(os.path.join(dirname, "solutions\model.ilp"))
+    stop = timeit.default_timer()
+
+    print('Time to solve the model: ',
+          f"{round(stop - start, 2)} seconds\n")
+
+
+    return model
+
+    """ For testing purposes, can be ignored
+    print("Set threshold as",MIN_PERCENTAGE)
+    k=0
+    tot= sum(I[i]["quantity"] for i in I)
+    for t in range(T):
+        val= int(sum([model.getVarByName(f'hp_type_{str(m)}_at_house_type_{str(i)}_in_year_{str(t)}').X for m in M for i in I]))
+        k=k+val
+
+        print("t=%02d "%(t,),": build", val,"houses", "with maximum houses", tot)
+    p=100*k/tot
+    print(p,"% wurde erfüllt beim Threshold ",MIN_PERCENTAGE)    
+
+    return
+    """
+
+
