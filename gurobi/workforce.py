0a65a7ccc81277ee65f99d97be7e3d336e05fc20
Xiaotong Meng <xiaotong.meng@rwth-aachen.de>
2022-06-30 19:48:29 +0200

    taskC

--- gurobi/workforce.py
+++ gurobi/workforce.py
@@ -4,7 +4,7 @@ import os
 
 
 dirname = os.path.dirname(__file__)
-DISTRIBUTOR = os.path.join(dirname, './data-sources/Distributor_data_with_coordinates.csv')
+DISTRIBUTOR = os.path.join(dirname, './data-sources/Distributor_data.csv')
 df_distributor = pd.read_csv(DISTRIBUTOR)
 
 # based on this https://www.isoenergy.co.uk/latest-news/renewable-energy-news-from-isoenergy/how-long-will-it-take-to-have-a-heat-pump- 
@@ -12,40 +12,55 @@ df_distributor = pd.read_csv(DISTRIBUTOR)
 # We assume that  the installation performed by 2 plumbers, 1 eletrician in the first week and then 2 tester in the second week. 
 
 
-#realistic assumption, however it takes more than T =60 years and can still not reach min_PERCENTAGE = 0.005 (model infeasible)
-# N3 = 15
-# N2 = 6
-# N1= 2
-# N0= 1
+#realistic assumption, however it takes more than T = 100 years and can still not reach min_PERCENTAGE = 0.005 (model infeasible)
+# N3 = 40
+# N2 = 10
+# N1= 4
+# N0= 3
 
-# minimum number of teams to make model with T =12, min_PERCENTAGE = 0.05 feasible, 
-# but assumption is not realistic, since in total there should be only 5000 employees of heat pump sector in total
+# # minimum number of teams to make model with T =12, min_PERCENTAGE = 0.05 feasible, 
+# # however, this assumption is not realistic, since in total there should be only < 6000 employees of heat pump sector.
 N3= N2 =N1= N0 = 95
 
 
 
-# those company can have 20 - 1000 employees, see
-# https://de.kompass.com/c/alban-technische-anlagen-gmbh/de157795/  
-# https://www.schrag-kantprofile.de/unternehmen/    
-# https://www.google.com/search?q=ait-deutschland+gmbh+mitarbeiter&sxsrf=ALiCzsZYKI-VLqM9_DSU13d9kXqeHV8R3w%3A1656279667118&ei=c9K4YqvoBqDc7_UP0YycqAY&oq=ait-deutschland+gmbh+mitar&gs_lcp=Cgdnd3Mtd2l6EAMYADIFCCEQoAEyBQghEKABMgUIIRCgAToHCAAQRxCwAzoECCMQJzoGCAAQHhAWOggIABAeEA8QFkoECEEYAEoECEYYAFCjBFjhC2CJEWgBcAF4AIABX4gBiAOSAQE2mAEAoAEByAEIwAEB&sclient=gws-wiz
-# assume that they have N3 installation team on average
-df_distributor.loc[df_distributor['operating radius'] == 800, 'workforce'] = N3
 
-#based on this  https://www.wer-zu-wem.de/firma/elmar-bey.html  44 employees
-# assume that all company with op_radius = 400 have N2 installation team on average
-df_distributor.loc[df_distributor['operating radius'] == 400, 'workforce'] = N2
+ 
+  
+# 
 
-# based on this http://www.cube-ing.de/firmenprofil.html#:~:text=Die%20Firma%20besch%C3%A4ftigt%2013%20Mitarbeiter,1%2C5%20Mio. 15 employees
-# assume that all company with op_radius = 200 have N1 installation team on average
-df_distributor.loc[df_distributor['operating radius'] == 200, 'workforce'] = N1
 
-# assume that all company with op_radius = 150 have N0 installation team on average
-df_distributor.loc[df_distributor['operating radius'] == 150, 'workforce'] = N0
+# http://www.wtq-lkk.de/ 45 employees
+#https://www.apollo-goessnitz.de/karriere/#:~:text=Mit%20%C3%BCber%20140%20Mitarbeitern%20am,zur%20Produktion%20von%20Systemanlagen%20und 140 employee
+# https://www.wer-zu-wem.de/firma/alfred-kaut-co.html 50-100 employees
+# assume that company with German-wide op_radius have N3 installation team on average
+df_distributor.loc[df_distributor['operating radius'].isna(), 'workforce'] = N3
+
+#  https://www.ait-deutschland.eu/startseite.html > 1000 employees
+#  https://die-deutsche-wirtschaft.de/famu_top/otto-schatte-gmbh-luebeck-umsatz-mitarbeiterzahl/ 215 employees
+#  https://www.wer-zu-wem.de/firma/elmar-bey.html  44 employees
+#  https://www.schrag-kantprofile.de/unternehmen/    500 employees
+# https://de.kompass.com/c/alban-technische-anlagen-gmbh/de157795/  20 employees
+
+# assume that all company with op_radius = 150 have N2 installation team on average
+df_distributor.loc[df_distributor['operating radius'] == 150, 'workforce'] = N2
+
+
+# http://www.cube-ing.de/firmenprofil.html#:~:text=Die%20Firma%20besch%C3%A4ftigt%2013%20Mitarbeiter,1%2C5%20Mio. 15 employees
+# assume that all company with op_radius = 100 have N1 installation team on average
+df_distributor.loc[df_distributor['operating radius'] == 100, 'workforce'] = N1
+
+
+#  https://de.kompass.com/c/secespol-deutschland-gmbh/de730240/  10-20 employees
+# # assume that all company with op_radius = 150 have N0 installation team on average
+df_distributor.loc[df_distributor['operating radius'] == 70, 'workforce'] = N0
 
 
 #adding variation
-n = df_distributor['operating radius'].value_counts().to_list()
+n = df_distributor['operating radius'].value_counts(dropna=False).to_list()
 print(n)
+
+
 print( str( (N3*n[3] + N2*n[2]+N1*n[1] + N0*n[0])*5 ) +   "   employees for German heat pump (installation) sector")
 
 
