f5c2cd3783a0c50c5335c5d648917a03d52bca34
PatrickSchro <105563390+PatrickSchro@users.noreply.github.com>
2022-07-19 11:50:27 +0200

    Update INSTANCES.py

--- gurobi/INSTANCES.py
+++ gurobi/INSTANCES.py
@@ -3,26 +3,27 @@ NUMBER_OF_YEARS = 15
 operating_radius = 70
 MIN_PERCENTAGE = 0 #actually not percentage :)
 CO2_EMISSION_GAS = 433 #g/kWh
-CO2_EMISSION_EON = 366 #g/kwh
+CO2_EMISSION_EON = 222 #g/kwh
 BOILER_EFFICIENCY = 0.7 #between 0 and 1
-GAS_PRIZE_FACTOR = 5 #Compared to Initial Value
-ELECTRICITY_PRICE_FACTOR = 0.2 #Compared to Initial Value
+GAS_PRIZE_FACTOR = 2.4 #Compared to Initial Value
+ELECTRICITY_PRICE_FACTOR = 1.21 #Compared to Initial Value
+CO2_EMISSION_FACTOR = 0.8
 ELECTRICITY_SUBS = 1
 HEATPUMP_SUBS = 1
-CO2_PRIZE_FACTOR = 5 #Compared to Initial Value
+CO2_PRIZE_FACTOR = 6.25 #Compared to Initial Value
 Max_Sales_Growth = 0.05 #per year
 Max_Sales_Initial = 110000 #units per year
 WORKFORCE_FACTOR = 2  # Compared to Initial value
 
 #Emission Prices can be either static welfare based values, or on the CO2 prices. We decided to consider more than the certificat price
 #then balanced with the welfare losses caused by climate change for current and future generations, alternatively 698E-6
-CO2_EMISSION_PRICE = 201E-6  # euro/gramm,  suggestion by German Environment Agency euro/gram,  
+CO2_EMISSION_PRICE = 80E-6  # euro/gramm, 2021 nEHS
 
 # from https://www.globalpetrolprices.com/Germany/natural_gas_prices/
 AVERAGE_BOILER_COST_PER_UNIT = 0.13
 
-# from https://www.eon.de/de/gk/strom/oekostrom.html#:~:text=Im%20Jahr%201990%20lag%20der,der%20CO%202%2DEmissionen%20leisten.
-ELECTRICITY_COST_PER_UNIT = 0.4805
+# from https://www.finanztip.de/stromvergleich/strompreis/#:~:text=Strom%20hat%20in%20Deutschland%20einen,und%20Wasserwirtschaft%20(BDEW)%20hervor.
+ELECTRICITY_COST_PER_UNIT = 0.3714
 
 # -*- coding: utf-8 -*-
 #INSTANCES
@@ -76,6 +77,7 @@ PARAMETERS = os.path.join(dirname, './data-sources/parameters.xlsx')
 electr_timefactor = np.linspace(1,ELECTRICITY_PRICE_FACTOR ,NUMBER_OF_YEARS) #The second factor is the multiplying factor for the final value, so the price is x times the starting price
 gas_timefactor = np.linspace(1,GAS_PRIZE_FACTOR ,NUMBER_OF_YEARS) #The second factor is the multiplying factor for the final value, so the price is x times the starting price
 CO2_timefactor = np.linspace(1,CO2_PRIZE_FACTOR ,NUMBER_OF_YEARS) #The second factor is the multiplying factor for the final value, so the price is x times the starting price
+CO2EMIS_timefactor = = np.linspace(1,CO2_EMISSION_FACTOR ,NUMBER_OF_YEARS) #The second factor is the multiplying factor for the final value, so the emission is x times the starting emission
 max_sales = npf.fv(Max_Sales_Growth, np.linspace(0,NUMBER_OF_YEARS+1,NUMBER_OF_YEARS, dtype = int), 0, -Max_Sales_Initial) #Third value is fixed addition
 # get prepared data
 
@@ -89,7 +91,7 @@ max_sales = npf.fv(Max_Sales_Growth, np.linspace(0,NUMBER_OF_YEARS+1,NUMBER_OF_Y
 model = modelsolver.solve(districts, heatpumps, housing, fitness, distributors, NUMBER_OF_YEARS, MIN_PERCENTAGE,
           CO2_EMISSION_GAS, CO2_EMISSION_EON, BOILER_EFFICIENCY, 
           CO2_EMISSION_PRICE, max_sales, AVERAGE_BOILER_COST_PER_UNIT, ELECTRICITY_COST_PER_UNIT,
-          electr_timefactor, gas_timefactor, CO2_timefactor, configurations, WORKFORCE_FACTOR, ELECTRICITY_SUBS, HEATPUMP_SUBS)
+          electr_timefactor, gas_timefactor, CO2_timefactor,CO2EMIS_timefactor, configurations, WORKFORCE_FACTOR, ELECTRICITY_SUBS, HEATPUMP_SUBS)
 
 write_solution_csv(model, districts, heatpumps, housing, NUMBER_OF_YEARS, distributors, NUMBER_OF_YEARS, MIN_PERCENTAGE,
                    CO2_EMISSION_GAS, CO2_EMISSION_EON, BOILER_EFFICIENCY,
