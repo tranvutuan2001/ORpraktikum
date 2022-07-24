a3a92e34e2ffa8231f93b1b37061d9d58cad26cc
joernmht <joern.Maurischat@T-Online.de>
2022-07-24 12:37:01 +0200

    Update printsolution.py
    Changed the rows to more informative ones

--- gurobi/printsolution.py
+++ gurobi/printsolution.py
@@ -25,7 +25,8 @@ def write_solution_csv(model, D, M, I, T, distributors, NUMBER_OF_YEARS, MIN_PER
         print("new file is created with headers")
         
         f = open(os.path.join(dirname, "solutions\solution.csv"), 'a', newline="")
-        header=["name","district", "year construct", "type of building", "modern", "HPModel", "Year","qty newly built HP","newpumps:percent of totalhouses", "cumulQTY", "cumul:percent of totalhouses","totalhouses","heatcapacity","distributor","zipcode"]
+        header=["name","district", "year construct", "type of building", "heat capacity", "HPModel", "Year","qty newly built HP", "cumul.QTY", "totalhouses", "percent of totalhouses", "average heat demand",
+         "distributor","zipcode"]
         writer = csv.writer(f, delimiter=";")
         writer.writerow(header)
         f.close()
@@ -49,7 +50,7 @@ def write_solution_csv(model, D, M, I, T, distributors, NUMBER_OF_YEARS, MIN_PER
         else:
             percent_new = round(new_installs / I[i]["quantity"],2)
             percent_cumul= round(cumulated_amount / I[i]["quantity"],2)
-            row = [name, I[i]["district"], I[i]["year of construction"], I[i]["type of building"], I[i]["modernization status"], M[m]['brand_name'], t,new_installs,percent_new,cumulated_amount, percent_cumul, I[i]["quantity"], I[i]["max_heat_demand_W/m^2"], distributors[d]['name'], I[i]["zipcode"]]
+            row = [name, I[i]["district"], I[i]["year of construction"], I[i]["type of building"], I[i]["Heatcapacity"], M[m]['brand_name'], t,newbuilds,qty, I[i]["quantity"], percent, I[i]["average heat demand"], distributors[d]['name'], I[i]["zipcode"]
             writer.writerow([r for r in row])
         
         if c==0:
